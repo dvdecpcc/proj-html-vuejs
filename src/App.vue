df8e695f0b5237d0a5fc21fcdaf1013fc9916d7d
davide <dacapoccia@gmail.com>
2022-09-05 20:30:57 +0200

    HelloWorld deleted

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    
+    <MyHeader/>
+    <MyMain/>
+    <MyFooter/>
   </div>
 </template>
 
 <script>
-import MyHeader from './components/HelloWorld.vue'
-import MyMain from './components/HelloWorld.vue'
-import MyFooter from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue';
+import MyMain from './components/MyMain.vue';
+import MyFooter from './components/MyFooter.vue';
 
 
 export default {
